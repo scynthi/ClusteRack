0e4a72b31a5d84630acfa57badd9ed925f425d9a
scynthi <85178488+scynthi@users.noreply.github.com>
2025-01-31 19:54:39 +0100

    Update test.py

--- test.py
+++ test.py
@@ -22,19 +22,26 @@ class PygameEmbedApp(AppWindow):
         pygame.init()
 
         hwnd = self.pygame_frame.winfo_id()
-        screen = pygame.display.set_mode((600, 400), pygame.NOFRAME)
+        screen = pygame.display.set_mode((600, 400), pygame.FULLSCREEN)
 
         if os.name == "nt":
             ctypes.windll.user32.SetParent(pygame.display.get_wm_info()["window"], hwnd)
+
+            style = ctypes.windll.user32.GetWindowLongW(pygame.display.get_wm_info()["window"], -16)
+            style &= ~0x00C00000
+            ctypes.windll.user32.SetWindowLongW(pygame.display.get_wm_info()["window"], -16, style)
+            ctypes.windll.user32.SetWindowPos(pygame.display.get_wm_info()["window"], None, 0, 0, 600, 400, 0x0004)
             self.after(100, self.position_pygame_top_left)
 
         self.run_pygame_loop(screen)
 
     def position_pygame_top_left(self):
+        print("yoooooooo")
         hwnd = pygame.display.get_wm_info()["window"]
-        frame_x, frame_y = self.pygame_frame.winfo_rootx(), self.pygame_frame.winfo_rooty()
 
-        ctypes.windll.user32.MoveWindow(hwnd, frame_x, frame_y, 600, 400, True)
+        ctypes.windll.user32.MoveWindow(hwnd, 0, 0, 0, 0, True)
+
+        self.after(100, lambda: app.position_pygame_top_left())
 
     def run_pygame_loop(self, screen):
         app = SoftwareRender()
@@ -46,9 +53,13 @@ class PygameEmbedApp(AppWindow):
                     return
 
             app.run()
+
             pygame.display.update()
             time.sleep(0.01)
 
 if __name__ == "__main__":
     app = PygameEmbedApp()
+    app.after(100, lambda: app.position_pygame_top_left())
     app.mainloop()
+
+
