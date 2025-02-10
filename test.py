ff765cdb207e5e0d6756cfc436f873b0e9f641db
PITRv1 <petersandor.toth@gmail.com>
2025-02-10 23:47:38 +0100

    Updated relocate process
    it now has 3 options to resolve merge conflicts

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ root : Root = Root(r"./Test folder")
 
 cluster1 : Cluster = root.clusters["cluster1"]
 
-root.relocate_process("chrome", "cluster1", "cluster2")
+# root.relocate_process("chrome", "cluster1", "cluster2")
 
 # Just write test code here
 # I cleaned it cause we dont use the commented stuff anyway
