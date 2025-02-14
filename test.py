ac0d902c60865473fc522e0e327fa456059cc604
PITRv1 <petersandor.toth@gmail.com>
2025-02-14 23:34:09 +0100

    Updated cluster still under heavy work

--- test.py
+++ test.py
@@ -3,60 +3,49 @@ from modules.cluster import Cluster
 from modules.root import Root
 from modules.interpreter import CLI_Interpreter
 
-root : Root = Root(r"./Test folder")
-cluster : Cluster = root.clusters.get("test_cluster")
-cluster1 : Cluster = root.clusters.get("test_cluster1")
+cluster = Cluster(r".\Test folder\cluster0")
 
-root.relocate_process("MAN", "test_cluster1", "test_cluster")
-
-cluster1.start_process("jani", False, 100,100,2)
-# cluster1.kill_process("jani")
-
-root.move_computer("test_computer", "test_cluster", "test_cluster1")
-
-# cluster1.rename_process("jani", "internet")
-# cluster1.edit_process_resources("internet", "running", True)
-
-# root.relocate_process("internet", "test_cluster1", "test_cluster")
-
-
-
+cluster.edit_instance("word-asd123", "computer", "computer1")
 
 """
 TODO nekem : mindenki jo uzenetet adjon vissza for CLI
-     
+            Check for computer name to only contain english charset and numbers
+            sub proc deletion by id
+            new 
 """
 
 """
-Featres:
-- root :
-- create cluster D
-- try delete cluster D
-- force delete cluster D
-- relocate process D
-- move computer D
-- rename cluster D
+Root
+Create cluster
+Delete cluster (try, force)
+Edit cluster name
+Move computer between clusters
+Move program between clusters
+Cleanup root
 
-- cluster:
-- set_default_rebalance_algo <load_balance (or) best_fit (or) fast> D
-- run_default_rebalance D
-- create computer D
-- try delete computer D
-- force delete computer D
-- edit computer resources D
-- rename computer D
-- start process D
-- kill process D
-- edit process resources D
-- rename process D
+Cluster
+Create computer 
+Del computer (try, force)
+Edit computer resources
+Rename computer 
+Set default rebalance algo
+Run rebalance
+Cleanup cluster
 
-- rebalancer:
-- balanced algo D
-- efficient algo D
-- fast algo D
+>>(Programok)
+     Start program
+     Stop program
+     Kill program
+     Edit program
+     Restart program
+    >>(Program instancek)
+         Start prog. instance
+         Stop prog. instance
+         Kill prog. instance
+         Move prog. instance
 
-- computer:
-- get processes D
+Computer:
+Cleanup computeri
 
 """
 
@@ -71,7 +60,7 @@ CLI Commands
 - create_cluster <cluster name>
 - try_del_cluster <cluster name>
 - force_del_cluster <cluster name>
-- relocate_process <process name> <origin> <destination>
+- relocate_program <program name> <origin> <destination>
 - move_computer <computer name> <origin> <destination>
 - rename_cluster <target name> <new name>
 
@@ -83,13 +72,17 @@ CLI Commands
 - try_del_computer <computer name>
 - force_del_computer <computer name>
 - rename_computer <target computer> <new name>
-- start_process <process name> <status> <req cores> <req memory> <instance count> <date started(optional)>
-- kill_process <process name>
-- edit_process_resources <process name> <prop to change> <new value>
-- rename_process <process name> <new name>
+- start_program <program name> <status> <req cores> <req memory> <instance count> <date started(optional)>
+- kill_program <program name>
+- edit_program_resources <program name> <prop to change> <new value>
+- rename_program <program name> <new name>
 
 
 - Computer comms:
 - edit_resources <core> <memory>
 - get_processes - Just gives back a dict of the subprocesses under the computer
+
+bnf
+https://en.wikipedia.org/wiki/Backus%E2%80%93Naur_form
+
 """
