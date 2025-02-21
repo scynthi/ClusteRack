3309a56f61eb495e54141daa765399f667548e6b
borokmate <borokmate@gmail.com>
2025-02-21 14:41:34 +0100

    aha ok

--- testest.py
+++ testest.py
@@ -2,7 +2,7 @@ import os
 import time
 
 # Set a unique title
-title = "FullscreenCMD"
+title = "Custom Command Interpreter"
 
 # Start the new cmd window with the unique title
 os.system(f'start cmd /c "title {title} && py interp_test.py"')
